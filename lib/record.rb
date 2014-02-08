90fc8a4c751a2f5d9b5e061a0543f600f4003fc6
Willa <mowmowjoker@gmail.com>
2014-02-08 14:42:07 -0500

    Added record class.

---
+++ lib/record.rb
@@ -0,0 +1,8 @@
+class Record
+  attr_accessor :last_name, :first_name, :gender, :favoriate_color, :date_of_birth
+  def initialize args
+    args.each do |k, v|
+      instance_variable_set("@#{k}", v)
+    end
+  end
+end
